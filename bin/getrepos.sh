26d174f9c646bceaf779ccb0f97defd62012678e
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-19 09:20:10 +0200

    add getrepos.sh

---
+++ bin/getrepos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Please provide a GitHub username as the first argument."
+  exit 1
+fi
+
+username=$1
+
+curl -s "https://api.github.com/users/${username}/repos?sort=updated&direction=desc" | jq '[.[] | {name: .name, url: .html_url}]'
+
